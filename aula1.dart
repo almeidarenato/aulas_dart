671f4ed9332cf8868f88c8bc688b06119f53e1c8
Renato Almeida <renatoam@ymail.com>
2021-12-12 17:56:44 -0300

    aula variaveis

--- aula1.dart
+++ aula1.dart
@@ -1,3 +1,15 @@
 void main(List<String> args) {
+  var hello = 'Hello World';
+  var numero = 3;
+  int outronumero = 7;
+  double numeroflutuante = 5.55;
+
   print('olá mundo');
+  print(10);
+  print(hello);
+  print('Valor da variavel: $hello');
+  print(numero);
+  print(numero + 2);
+  print(outronumero + 1);
+  print(numeroflutuante + 3.1);
 }
