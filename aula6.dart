f872e6e5f96ddf1e91c7b89fb40ec9a855e080db
Renato Almeida <renatoam@ymail.com>
2021-12-12 20:33:45 -0300

    arrays , funcões e tipagem

---
+++ aula6.dart
@@ -0,0 +1,21 @@
+main() {
+  var nomes = []; //array de variaveis
+
+  nomes = ['Renato', 'Rodrigo'];
+  print(nomes);
+
+  nomes.add('Flavia');
+  print(nomes);
+  print(nomes.length); //tamanho de itens Array
+  print(nomes[0]); // retorna o item na posição zero
+  nomes.remove('Rodrigo'); //remove o primeiro item com o nome encontrado;
+  print(nomes);
+  nomes.add('Rodrigo');
+  print(nomes);
+  nomes.removeLast(); //remove ultimo item
+  print(nomes);
+  nomes.removeAt(0); //remove item na posiçao indicada
+  print(nomes);
+
+  List<String> listaNomes = []; //lista com tipagem informada.
+}
